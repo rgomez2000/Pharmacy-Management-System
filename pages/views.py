06caca22b2007af33cf2ad2da462d39561e7d237
starsvoyage <idiazvachier@arizona.edu>
2024-09-29 20:39:10 -0700

    Added Home and About pages

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_view(*args, **kwargs):
+    return "<h1> Home Page </h1>"
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h1> About </h1>")
