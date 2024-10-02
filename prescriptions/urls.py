c0da13ac69970ac5bbfae0abbf70767456fe006a
Ryan Davis <rad354@nau.edu>
2024-10-02 06:01:54 -0700

    Adds a new 'add' page, and updates rendering logic

--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.prescriptions_main, name="prescriptions"),
     path("all/", views.all, name="all"),
+    path("add/", views.add, name="add"),
     path("details/<int:id>/", views.details, name="details")
 ]
--- prescriptions/views.py
+++ prescriptions/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from .models import Prescription
 
 def all(request):
     my_prescriptions = Prescription.objects.all().values()
-    template = loader.get_template('all_prescriptions.html')
     context = {
         "my_prescriptions" : my_prescriptions
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "all_prescriptions.html", context)
 
 def details(request, id):
     my_prescription = Prescription.objects.get(id=id)
-    template = loader.get_template('details.html')
     context = {
         "prescription" : my_prescription
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, "details.html", context)
 
 def prescriptions_main(request):
-    template = loader.get_template('main.html')
-    return HttpResponse(template.render())
+    return render(request, 'main.html')
+
+def add(request):
+    return render(request, 'add.html')
