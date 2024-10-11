da3267e21e317f3c55ee1afc37ef5b31e22d81c2
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2024-10-11 02:15:31 -0700

    Added prescription entering, prescription viewing

--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.prescriptions_main, name="prescriptions"),
-    path("all/", views.all, name="all"),
-    path("add/", views.add, name="add"),
-    path("details/<int:id>/", views.details, name="details")
+    path("add-prescription/", views.add_prescription, name="add_prescription"),
+    path('prescriptions/', views.prescription_list, name='prescription_list'),
 ]
--- prescriptions/views.py
+++ prescriptions/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from .forms import PrescriptionForm
 from .models import Prescription
 
-def all(request):
-    my_prescriptions = Prescription.objects.all().values()
-    context = {
-        "my_prescriptions" : my_prescriptions
-    }
-    return render(request, "all_prescriptions.html", context)
+@login_required
+def add_prescription(request):
+    if request.method == 'POST':
+        form = PrescriptionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('prescriptions')
+    else:
+        form = PrescriptionForm()  # Ensure the form is being created
 
-def details(request, id):
-    my_prescription = Prescription.objects.get(id=id)
-    context = {
-        "prescription" : my_prescription
-    }
-    return render(request, "details.html", context)
+    return render(
+        request, 'add_prescription.html', {'form': form}
+    )  # Ensure the form is passed to the template
 
 def prescriptions_main(request):
     return render(request, 'main.html')
 
-def add(request):
-    return render(request, 'add.html')
+def prescription_list(request):
+    prescriptions = Prescription.objects.all()  # Retrieve all prescriptions
+    return render(request, 'prescription_list.html', {'prescriptions': prescriptions})
