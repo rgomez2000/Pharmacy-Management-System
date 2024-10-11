bfc3f03f744448cd2d157746fd739db7f93c6943
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2024-10-11 03:28:36 -0700

    Added pharmacist, pharmacy manager capabilities

--- prescriptions/views.py
+++ prescriptions/views.py
@@ -1,9 +1,19 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import redirect, render, get_object_or_404
 from .forms import PrescriptionForm
 from .models import Prescription
 
+def is_pharmacist(user):
+    return user.groups.filter(name='Pharmacist').exists()
+
+def is_pharmacy_manager(user):
+    return user.groups.filter(name='Pharmacy Manager').exists()
+
+def is_pharmacy_technician(user):
+    return user.groups.filter(name='Pharmacy Technician').exists()
+
 @login_required
+@user_passes_test(is_pharmacist)
 def add_prescription(request):
     if request.method == 'POST':
         form = PrescriptionForm(request.POST)
@@ -17,13 +27,19 @@ def add_prescription(request):
         request, 'add_prescription.html', {'form': form}
     )  # Makes sure the form is passed to the template
 
+@login_required
+@user_passes_test(is_pharmacist, is_pharmacy_manager)
 def prescriptions_main(request):
     return render(request, 'main.html')
 
+@login_required
+@user_passes_test(is_pharmacist, is_pharmacy_manager)
 def prescription_list(request):
     prescriptions = Prescription.objects.all()  # Retrieves all of the prescriptions
     return render(request, 'prescription_list.html', {'prescriptions': prescriptions})
 
+@login_required
+@user_passes_test(is_pharmacist)
 def delete_prescription(request, pk):
     # Get the prescription by primary key (pk)
     prescription = get_object_or_404(Prescription, pk=pk)
@@ -37,6 +53,8 @@ def delete_prescription(request, pk):
     
     return render(request, 'confirm_delete.html', {'prescription': prescription})
 
+@login_required
+@user_passes_test(is_pharmacist)
 def edit_prescription(request, pk):
     prescription = get_object_or_404(Prescription, pk=pk)
 
