d0547eed116dac86653a3da42c23be45567d31e9
rgomez2000 <83112451+rgomez2000@users.noreply.github.com>
2024-11-01 23:49:58 -0700

    Added the Fill Prescription form and restricted access to only Pharmacists.

--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.prescriptions_main, name="prescriptions"),
     path("add-prescription/", views.add_prescription, name="add_prescription"),
     path('prescriptions/', views.prescription_list, name='prescription_list'),
+    path('prescriptions/fill/<int:pk>/', views.fill_prescription, name='fill_prescription'),
     path('prescriptions/delete/<int:pk>/', views.delete_prescription, name='delete_prescription'), 
     path('prescriptions/edit/<int:pk>/', views.edit_prescription, name='edit_prescription'),  
 ]
