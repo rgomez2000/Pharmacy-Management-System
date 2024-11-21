0682e045fa2b4a902ed74f2bfda51e0ecdeefabc
jdm41818 <jdm41818@arizona.edu>
2024-11-20 21:57:12 -0700

    This push adds the payment type functionality for the transactions as well as creates a reciept with the information needed from the transaction. The reciept gets stored in the database but is also printable

--- cashier/templatetags/get_details.py
+++ cashier/templatetags/get_details.py
@@ -7,3 +7,13 @@ def get_item_details(item, purchase):
     # Returns the PurchasedItemDetails object associated with the
     # instance of `item` and `purchase`
     return item.purchaseditemdetails_set.get(purchase=purchase)
+
+@register.filter
+def mul(value, arg):
+    #Multiply the given value by the argument.
+    #Usage: {{ value|mul:arg }}
+    
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return 0
